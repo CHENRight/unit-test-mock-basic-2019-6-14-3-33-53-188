35a0539779437cf15653bb22c95806b6e6421916
REET CHEN (ITA-ISDC-ISD-OOCLL/ZHA) <CHENRE6@OOCL.COM>
2019-07-15 21:03:32 +0800

    implement by use mock

--- src/test/java/cashregister/CashRegisterTest.java
+++ src/test/java/cashregister/CashRegisterTest.java
@@ -1,32 +1,65 @@
 package cashregister;
 
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.*;
 
 public class CashRegisterTest {
 
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    private final PrintStream outStream = System.out;
+
+    @BeforeEach
+    void setUpStream(){
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @AfterEach
+    void restoreStreams(){
+        System.setOut(outStream);
+    }
 
     @Test
     public void should_print_the_real_purchase_when_call_process() {
-        //given
-        //when
-        //then
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        Item item = new Item("Coco",1.0);
+        Item[] items = new Item[]{item};
+        Purchase purchase = new Purchase(items);
+        Printer printer = new Printer();
+        CashRegister cashRegister = new CashRegister(printer);
+        cashRegister.process(purchase);
+        Assertions.assertEquals("Coco\t1.0\n",outContent.toString());
     }
 
     @Test
     public void should_print_the_stub_purchase_when_call_process() {
-        //given
-        //when
-        //then
+
+        Purchase mockPurchase = mock(Purchase.class);
+        CashRegister cashRegister = new CashRegister(new Printer());
+
+        when(mockPurchase.asString()).thenReturn("purchase mock");
+        cashRegister.process(mockPurchase);
+
+        assertThat(outputStream.toString()).isEqualTo("purchase mock");
     }
 
     @Test
     public void should_verify_with_process_call_with_mockito() {
-        //given
-        //when
-        //then
+        Printer mockPrinter = mock(Printer.class);
+        Purchase mockPurchase = mock(Purchase.class);
+        CashRegister cashRegister = new CashRegister(mockPrinter);
+
+        when(mockPurchase.asString()).thenReturn("purchase mock");
+        cashRegister.process(mockPurchase);
+        verify(mockPurchase).asString();
+
     }
 
 }
